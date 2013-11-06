c3c4af99160fa60e0964520d637290ae2bae0212
citymont <thibaut@moderntree.net>
2013-11-06 17:45:20 +0100

    add method get list/one to history.php

--- admin/lib/history.php
+++ admin/lib/history.php
@@ -2,25 +2,64 @@
 
 /*
 	Enregistre l'historique d'edition d'une page
+	POST : save current file
+	GET : get history files
 
  */
 
-$model =$_POST['model'];
-$id =$_POST['id']; 
-$dossier = __DIR__."/../../app/data/".$model."/";
-if(!$id) { 
-	$file = $dossier.time().'.json';
+if($_SERVER['REQUEST_METHOD'] === "POST") {
+
+	$model =$_POST['model'];
+	$id =$_POST['id']; 
+	$dossier = __DIR__."/../../app/data/".$model."/";
+	if(!$id) { 
+		$file = $dossier.time().'.json';
+	} else {
+		$file = $dossier.$id.'/'.time().'.json';
+	}
+
+	$data =$_POST['data'];
+
+	file_put_contents($file, $data);
+	if(!$id) {
+		$fileIndex = $dossier.'choose'.'.json';
+	} else {
+		$fileIndex = $dossier.$id.'/'.'choose'.'.json';
+	}
+
+	file_put_contents($fileIndex, $data);
+
 } else {
-	$file = $dossier.$id.'/'.time().'.json';
+	$apiMethod =$_GET['method'];
+
+	$model =$_GET['model'];
+	$id =$_GET['id']; 
+	$dossier = __DIR__."/../../app/data/".$model."/";
+
+	if($apiMethod == "list") {
+
+		$files = scandir($dossier, 1);
+
+		$output ="<ul>";
+
+		foreach ($files as $value) {
+			if($value === '.' || $value === '..' || $value === 'choose.json') {continue;} 
+			$output .= "<li>".$value."</li>";
+		}
+
+		$output .="</ul>";
+		print $output;
+
+	}
+
+	if($apiMethod == "one") {
+
+		if(!$id) {
+			$fileIndex = $dossier.$_GET['file'];
+		} else {
+			$fileIndex = $dossier.$id.'/'.$_GET['file'];
+		}
+
+		print file_get_contents($fileIndex);
+	}
 }
-
-$data =$_POST['data'];
-
-file_put_contents($file, $data);
-if(!$id) {
-	$fileIndex = $dossier.'choose'.'.json';
-} else {
-	$fileIndex = $dossier.$id.'/'.'choose'.'.json';
-}
-
-file_put_contents($fileIndex, $data);
