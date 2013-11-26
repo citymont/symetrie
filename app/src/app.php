095ed47bdd3bf5d1c0d0217c868976a1cb28ff6f
thibaut <t.villemont@troiscube.com>
2013-11-26 17:51:30 +0100

    fix parseUrl : index.php/ == / and  route "/" = model index by default

--- app/src/app.php
+++ app/src/app.php
@@ -6,22 +6,46 @@
 class App {
 
 	public $loginKey = "aa3261152486caad6c230b4b6d384361";
+
+	private $routes = array(
+		    "/" => "IndexHandler",
+		    "/:alpha" => "IndexHandler",
+		    "/index/:alpha/:alpha" => "IndexHandler"
+		);
+
+	private $routesAdmin = array(
+			"/admin/history" => "AdminHistoryHandler",
+		    "/admin/login" => "AdminLoginHandler",
+		    "/admin/logout" => "AdminLogoutHandler");
+
 	/**
 	 * Fonction de parsage de l'URI courante
 	 * @param  string $extra ajouter un / pour accéder au dossier
 	 * @return array        explosion de chaine
 	 */
-	public static function parseUrl($extra = null) {
+	public function parseUrl($extra = null) {
 
 		$serv = $_SERVER['REQUEST_URI']; 
-		$a = (defined('ADMIN')) ? explode("admin.php/", $serv) : explode("index.php/", $serv);
-		$b = explode("/", $a[1]);
-		$model = $b[0];
+
+		if(count(explode("index.php/", $serv)) == 2) {
+			$a = (defined('ADMIN')) ? explode("admin.php/", $serv) : explode("index.php/", $serv);
+			$b = explode("/", $a[1]);
+			$model = $b[0];
+		} else { 
+			$a = explode("/", $serv);
+			$b = explode("/", $a[1]);
+			$model = $b[0];
+		}
 
 		if(isset($b[1])) {
 			$id = $b[1];
 		} else { 
 			$id=""; 
+		}
+
+		$routes = $this->routes;
+		if (array_key_exists('/', $routes)) {
+		    $model = "index";
 		}
 
 		return array('model' => $model, 'id' => $extra.$id);
@@ -39,16 +63,8 @@ class App {
     		exit;
 		});
 
-		$routes = array(
-		    "/index" => "IndexHandler",
-		    "/index/:alpha" => "IndexHandler",
-		    "/index/:alpha/:alpha" => "IndexHandler"
-		);
-
-		$routesAdmin = array(
-			"/admin/history" => "AdminHistoryHandler",
-		    "/admin/login" => "AdminLoginHandler",
-		    "/admin/logout" => "AdminLogoutHandler");
+		$routes = $this->routes;
+		$routesAdmin = $this->routesAdmin;
 		
 		if( defined('ADMIN') ) { 
 			$routes = array_merge($routes, $routesAdmin);
