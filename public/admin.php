25fc60d4de000f32d7dec6e35e7c45d35fe2083d
citymont <thibaut@moderntree.net>
2014-01-12 18:09:02 +0100

    less code on start files (admin/index)

--- public/admin.php
+++ public/admin.php
@@ -2,51 +2,11 @@
 if( !session_id() ) session_start();
 define("ADMIN",true);
 
-require(__DIR__."/../app/src/conf.php");
-require(__DIR__."/../vendor/torophp/torophp/src/Toro.php");
-require(__DIR__."/../app/src/actions.php");
-require(__DIR__."/../app/src/index.actions.php");
-require(__DIR__."/../app/src/admin/history.actions.php");
-require(__DIR__."/../app/src/admin/login.actions.php");
-require(__DIR__."/../app/src/admin/logout.actions.php");
-require(__DIR__."/../app/src/app.php"); 
-
-/* ---- */
+require(__DIR__."/../app/conf.php");
+require_once(__DIR__."/../app/actions/autoload.php");
 
 $app = new App();
 
-ToroHook::add("before_request", function($vars) { 
-
-	if( isset($_GET['loginkey']) ) {
-		
-		// Test login page
-		$loginkeyTest = new App();
-		$loginkeyTest->testLoginUri("admin/login");
-		
-	} else {
-		$login = new App();
-		if( isset($_SESSION['role']) and isset($_SESSION['key'])) {
-			if ($_SESSION['role'] = 'ADMIN' and md5($_SESSION['key']) == $login->loginKey ) {
-
-			} else {
-				$login->error401();
-			}
-		} else {
-			$login->error401();
-		}
-}
-	
-
-});
-
-ToroHook::add("before_handler", function($vars) { });
-
-ToroHook::add("after_handler", function() { 
-
-	$app = new App();
-	print $app->getFlash();
-
-});
-
+$app->startPrivateAdmin();
 
 $app->startRoutes();
--- public/index.php
+++ public/index.php
@@ -1,46 +1,10 @@
 <?php
 
-require(__DIR__."/../app/src/conf.php");
-require(__DIR__."/../vendor/torophp/torophp/src/Toro.php");
-require(__DIR__."/../app/src/cache.php");
-require(__DIR__."/../app/src/actions.php");
-require(__DIR__."/../app/src/app.php");
-require(__DIR__."/../app/src/index.actions.php");
-
-/* ---- */
+require_once(__DIR__."/../app/conf.php");
+require_once(__DIR__."/../app/actions/autoload.php");
 
 $app = new App();
 
-ToroHook::add("before_handler", function($vars) { 
-
-	$appCache = new Cache(); 
-	
-	$cacheName = md5($_SERVER['REQUEST_URI']);
-	$cache = __DIR__.'/../app/storage/cache/'.$cacheName.'.cache.html';
-
-    if($appCache->check_cache($cache) == true) {
-		readfile($cache);	 
-	}
-	else { 
-		define("CACHE_FLAG", true); 
-		$appCache->start();
-
-	}
-});
-
-ToroHook::add("after_handler", function() { 
-
-	if( defined('CACHE_FLAG') ) { 
-
-		$appCache = new Cache(); 
-		$cacheName = md5($_SERVER['REQUEST_URI']);
-		$cache = __DIR__.'/../app/storage/cache/'.$cacheName.'.cache.html';
-
-		$cachecontent = ob_get_contents();
-
-		$appCache->end($cache,$cachecontent);
-	}
-
-});
+$app->startCache();
 
 $app->startRoutes();
