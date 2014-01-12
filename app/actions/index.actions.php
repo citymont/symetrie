08b3acab5681a6aea4dd83f64e2d935fc627f151
citymont <thibaut@moderntree.net>
2014-01-12 18:04:48 +0100

    more elegant model.actions class

---
+++ app/actions/index.actions.php
@@ -0,0 +1,39 @@
+<?php
+
+class IndexHandler {
+
+	private $modelName = "";
+	private $docId = "";
+
+    function __construct() {
+
+    	$app = new App();
+    	$infos = $app->getRouteInfos('/');
+    	$this->modelName = $infos['model'];
+    	$this->docId = $infos['id'];
+
+    }
+
+    function get($name = null, $b = null) {
+
+    	$appActions = new Actions(); 
+
+		    if( defined('CACHE_FLAG') ) { 
+		    	
+				$twig = $appActions->Twigloader();
+
+				$appActions->renderView($twig, $this->modelName,$this->docId);
+
+			}
+
+			if( defined('ADMIN') ) { 
+				
+				$appActions->Admin($this->modelName); 
+		
+			}
+
+      
+    }
+}
+
+
