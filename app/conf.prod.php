4143eaf00275bd6f9528c806c76eb7a025e48e1e
Thibaut villemont <citymont@gmail.com>
2016-04-21 16:23:41 +0200

    conf file for prod

---
+++ app/conf.prod.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(__DIR__."/lib/app.php");
+
+class App extends AppOrigin {
+
+	public function __construct() 
+    { 
+        // Key login
+        $this->loginKey = "CHANGE"; 
+
+        // Routes
+        $this->routes = array(
+		    "/" => "IndexHandler"
+		);
+
+        // Twig Conf Assets
+        $this->assets = "/public/assets/";
+        $this->adminAssets = "/public/assets/admin/";
+        $this->adminConfBase = "/public/admin.php";
+
+        // Dev Mode : model autoparser
+        $this->devMode = false;
+
+        // Cache Expire
+        $this->cacheExpire = 10000;
+        $this->cacheTwig = true;
+
+    }
+
+
+}
